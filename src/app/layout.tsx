86092bb18faf55b8e260f3e3fa859349f3820dae
Jaden Altitude <jadenaltitude5000@gmail.com>
2025-09-01 19:04:07 +0000

    can you make the app  faster to run? especially opening pages..maybe pre

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import { AppSidebar } from "@/components/layout/sidebar";
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 import { GlobalSearch } from "@/components/layout/global-search";
 import { ClientOnly } from "@/components/layout/client-only";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "Sentry",
@@ -19,19 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="font-body antialiased">
+      <body className={`${inter.variable} font-body antialiased`}>
         <SidebarProvider>
           <ClientOnly>
             <AppSidebar />
