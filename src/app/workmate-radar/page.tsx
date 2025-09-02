8a11a1c00422c99b2e40c2bc8c8d7d89620d8927
Jaden Altitude <jadenaltitude5000@gmail.com>
2025-09-02 12:38:51 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="relati

--- src/app/workmate-radar/page.tsx
+++ src/app/workmate-radar/page.tsx
@@ -6,24 +6,22 @@ import { ClientOnly } from "@/components/layout/client-only";
 export default function WorkmateRadarPage() {
   return (
     <div>
-      <header className="relative flex h-screen flex-col items-center justify-center overflow-hidden">
+      <header className="relative flex h-screen flex-col items-center justify-center gap-12 overflow-hidden">
           {/* Radar Animation */}
-          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-              <div className="relative flex h-80 w-80 items-center justify-center">
-                  {/* Radar Pings */}
-                  <div className="absolute h-full w-full rounded-full bg-primary/5 animate-ping [animation-duration:3s] [animation-delay:0s]"></div>
-                  <div className="absolute h-[calc(100%-40px)] w-[calc(100%-40px)] rounded-full bg-primary/5 animate-ping [animation-duration:3s] [animation-delay:0.5s]"></div>
-                  <div className="absolute h-[calc(100%-80px)] w-[calc(100%-80px)] rounded-full bg-primary/5 animate-ping [animation-duration:3s] [animation-delay:1s]"></div>
-                  
-                  {/* Radar Static Rings */}
-                  <div className="absolute h-20 w-20 rounded-full border-2 border-dashed border-primary/20"></div>
-                  <div className="absolute h-40 w-40 rounded-full border-2 border-dashed border-primary/20"></div>
-                  <div className="absolute h-60 w-60 rounded-full border-2 border-dashed border-primary/20"></div>
-                  <div className="absolute h-80 w-80 rounded-full border-2 border-dashed border-primary/20"></div>
-                  
-                  {/* Center Dot */}
-                  <div className="h-4 w-4 rounded-full bg-primary shadow-lg"></div>
-              </div>
+          <div className="relative flex h-80 w-80 items-center justify-center">
+              {/* Radar Pings */}
+              <div className="absolute h-full w-full rounded-full bg-primary/5 animate-ping [animation-duration:3s] [animation-delay:0s]"></div>
+              <div className="absolute h-[calc(100%-40px)] w-[calc(100%-40px)] rounded-full bg-primary/5 animate-ping [animation-duration:3s] [animation-delay:0.5s]"></div>
+              <div className="absolute h-[calc(100%-80px)] w-[calc(100%-80px)] rounded-full bg-primary/5 animate-ping [animation-duration:3s] [animation-delay:1s]"></div>
+              
+              {/* Radar Static Rings */}
+              <div className="absolute h-20 w-20 rounded-full border-2 border-dashed border-primary/20"></div>
+              <div className="absolute h-40 w-40 rounded-full border-2 border-dashed border-primary/20"></div>
+              <div className="absolute h-60 w-60 rounded-full border-2 border-dashed border-primary/20"></div>
+              <div className="absolute h-80 w-80 rounded-full border-2 border-dashed border-primary/20"></div>
+              
+              {/* Center Dot */}
+              <div className="h-4 w-4 rounded-full bg-primary shadow-lg"></div>
           </div>
           
           {/* Page Title */}
