6d9f90bdfb64ebf0aab49af0dffa19d1a8b7f42a
Jaden Altitude <jadenaltitude5000@gmail.com>
2025-09-05 16:02:43 +0000

    change the text here only to "WORKFLOW" (_for element span_)

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -75,7 +75,7 @@ export function AppSidebar() {
                 <svg className="size-7" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-2-9h4v2h-4v-2zm0-3h4v2h-4v-2z"/>
                 </svg>
-                <span className="duration-200 group-data-[collapsible=icon]:-ml-8 group-data-[collapsible=icon]:opacity-0">SENTRY</span>
+                <span className="duration-200 group-data-[collapsible=icon]:-ml-8 group-data-[collapsible=icon]:opacity-0">WORKFLOW</span>
             </div>
         </div>
       </SidebarHeader>
