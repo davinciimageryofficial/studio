85db56babede0201516d73bb07806a415fac3626
Jaden Altitude <jadenaltitude5000@gmail.com>
2025-09-10 08:49:54 +0000

    make slightly transparent (_for element <GlobalSearch>_)

--- src/components/layout/global-search.tsx
+++ src/components/layout/global-search.tsx
@@ -130,7 +130,7 @@ export function GlobalSearch() {
 
   return (
     <div className={cn(
-        "sticky top-0 z-30 w-full border-b bg-background/60 py-2 backdrop-blur-xl transition-all duration-300", 
+        "sticky top-0 z-30 w-full border-b bg-background/80 py-2 backdrop-blur-xl transition-all duration-300", 
         showResults && "h-[calc(100vh-8rem)]"
     )}>
         <div className={cn("container flex items-center justify-center gap-4 px-4", showResults && "flex-col h-full")}>
@@ -286,3 +286,5 @@ export function GlobalSearch() {
     </div>
   );
 }
+
+    
