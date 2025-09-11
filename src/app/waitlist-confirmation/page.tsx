61b16b330f37173c32b970abb072b1d18c8eae8a
Jaden Altitude <jadenaltitude5000@gmail.com>
2025-09-11 10:56:42 +0000

    make the edge invisible (_for element div_)

--- src/app/waitlist-confirmation/page.tsx
+++ src/app/waitlist-confirmation/page.tsx
@@ -85,7 +85,7 @@ export default function WaitlistConfirmationPage() {
                         </CardDescription>
                     </CardHeader>
                     <CardContent className="px-6 pb-6 space-y-2">
-                        <div className="space-y-3 rounded-lg border bg-card p-4 text-left border-black">
+                        <div className="space-y-3 rounded-lg bg-card p-4 text-left">
                         <h3 className="font-semibold text-md mb-2">Your Submitted Information:</h3>
                         <div className="flex items-center gap-4">
                             <User className="h-5 w-5 text-muted-foreground" />
